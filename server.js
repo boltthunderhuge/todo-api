3b877c3eaea6cec50cf3018369bd9b9082a22af2
Graeme Anderson <graemeanderson@kabaminc.com>
2016-07-14 14:21:30 -0700

    add todos and todos:id routes

--- server.js
+++ server.js
@@ -2,8 +2,41 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	description: 'Meet Mumsie for lunch',
+	completed: false
+},
+{
+	id: 2,
+	description: 'go to market',
+	completed: false
+},
+{
+	id: 3,
+	description: 'do this task',
+	completed: true
+}];
+
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
+});
+
+app.get('/todos', function(req, res) {
+	res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res) {
+	var todoID = req.params.id;
+
+console.log("Trying to match " + todoID);
+	for (var i=0; i<todos.length; ++i) {
+		if (todos[i].id == todoID) {
+			res.json(todos[i]);
+			return;
+		}
+	}
+	res.status(404).send('Fuck-up');
 });
 
 app.listen(PORT, function() {
