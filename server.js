8c352d83aed68ec401b5d62e920ec0c09a69faca
Graeme Anderson <graemeanderson@kabaminc.com>
2016-07-15 15:46:17 -0700

    Added PUT route for updating todos with specified id

--- server.js
+++ server.js
@@ -59,6 +59,42 @@ app.delete('/todos/:id', function(req, res) {
 	}
 });
 
+// PUT /todos/:id 
+app.put('/todos/:id', function(req, res) {
+	// Make sure the todo in the request has only the fields we want.
+	var todoID = parseInt(req.params.id, 10);
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	var matchedTodo = _.findWhere(todos, {id: todoID});
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+	
+	console.log("Here 1");
+	console.log(body);
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		// bad things
+		return res.status(400).send();
+	}
+
+	console.log("Here 2");
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length >= 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		// bad
+		return res.status(400).send();
+	}
+
+	console.log("Here 3");
+	console.log(validAttributes);
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+});
+
 app.listen(PORT, function() {
 	console.log("Express listening on PORT : " + PORT);
 });
