2f0d28963627a0b736bed85bde811d304d58de9d
Graeme Anderson <graemeanderson@kabaminc.com>
2016-07-18 10:35:12 -0700

    Added GET by id route

--- server.js
+++ server.js
@@ -42,15 +42,16 @@ app.get('/todos', function(req, res) {
 app.get('/todos/:id', function(req, res) {
 	var todoID = parseInt(req.params.id, 10);
 
-	matchedTodo = _.findWhere(todos, {
-		id: todoID
+	db.todo.findById(todoID).then(function(todo) {
+		if (!!todo) {
+			res.status(200).json(todo.toJSON());
+		} else {
+			res.sendStatus(404);
+		}
+	}, function(error) {
+		res.status(500).json(error);
 	});
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send("fuck up");
-	}
 });
 
 app.post('/todos', function(req, res) {
