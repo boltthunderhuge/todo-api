9e2b4374cc17e65eb77ffecc58893fd8079fcecc
Graeme Anderson <graemeanderson@kabaminc.com>
2016-07-19 14:34:25 -0700

    Update PUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -105,39 +105,29 @@ app.put('/todos/:id', function(req, res) {
 	// Make sure the todo in the request has only the fields we want.
 	var todoID = parseInt(req.params.id, 10);
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttributes = {};
-	var matchedTodo = _.findWhere(todos, {
-		id: todoID
+	var attributes = {};
+
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
+	}
+
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
+	}
+
+	db.todo.findById(todoID).then(function(todo) {
+		if (todo) {
+			 todo.update(attributes).then(function(todo) {
+				res.json(todo.toJSON());
+			}, function(error) {
+				res.status(400).json(error);
+			});
+		} else {
+			res.status(404).send();
+		}
+	}, function() {
+		res.status(500).send();
 	});
-
-	if (!matchedTodo) {
-		return res.status(404).send();
-	}
-
-	console.log("Here 1");
-
-	console.log(body);
-
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		// bad things
-		return res.status(400).send();
-	}
-
-	console.log("Here 2");
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length >= 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		// bad
-		return res.status(400).send();
-	}
-
-	console.log("Here 3");
-	console.log(validAttributes);
-
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
 });
 
 
